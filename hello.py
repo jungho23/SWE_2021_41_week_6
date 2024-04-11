d519ead65e0ebbbc53c08de5fb1279623982b032
jungho23 <junghokim97@gmail.com>
2024-04-11 09:49:45 +0000

    init: hello.py

---
+++ hello.py
@@ -0,0 +1,10 @@
+from typing import List
+
+def twoSum(nums: List[int], target: int) -> List[int]:
+
+    for i in range(len(nums)):
+        for j in range(len(nums)):
+            if i==j:
+                continue
+            if nums[i] + nums[j] == target:
+                return [i,j]
